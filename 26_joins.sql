b27bf5bc7636a9b9c5b020f7feedb0fd2a8871bc
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-03-31 14:54:38 -0300

    Agrega query left join

--- 26_joins.sql
+++ 26_joins.sql
@@ -15,3 +15,16 @@ INNER JOIN ventas v ON l.agente_id = v.agente_id AND l.cliente_id = v.cliente_id
 INNER JOIN agentes a ON l.agente_id = a.agente_id 
 INNER JOIN clientes c ON l.cliente_id = c.cliente_id;
 
+-- 📝 Ejercicio 2: LEFT JOIN
+-- Pregunta:
+-- "Queremos saber qué agentes NO han realizado ninguna venta. Muestra su nombre y fecha de contratación."
+
+-- Tu tarea:
+-- Usa LEFT JOIN para resolverlo.
+
+SELECT
+    a.nombre AS nombre_agente,
+    a.fecha_contratacion
+FROM agentes a 
+LEFT JOIN ventas v ON a.agente_id = v.agente_id 
+WHERE v.venta_id IS NULL;
