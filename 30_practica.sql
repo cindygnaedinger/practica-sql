18aa3753f195df7d68e15c6aa6379e8fc790de9b
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-04-05 11:50:08 -0300

    Agrega consulta de practica

--- 30_practica.sql
+++ 30_practica.sql
@@ -10,3 +10,13 @@ INNER JOIN (
   WHERE amount > 500
 ) o ON u.user_id = o.user_id
 ORDER BY u.name;
+
+-- "Encuentra a los usuarios (name) cuyo gasto total (suma de amount) en órdenes sea mayor a $1000. Muestra su nombre y gasto total, ordenado de mayor a menor gasto."
+SELECT 
+  u.name, 
+  SUM(o.amount) AS total_gasto
+FROM ecommerce.users u
+INNER JOIN ecommerce.orders o ON u.user_id = o.user_id
+GROUP BY u.name
+HAVING SUM(o.amount) > 1000
+ORDER BY total_gasto DESC;
