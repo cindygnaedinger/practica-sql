d6e3b646c0935887c03388b8d731f0a87c133ff2
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-04-06 13:57:55 -0300

    Agrega consulta de practica

--- 30_practica.sql
+++ 30_practica.sql
@@ -20,3 +20,15 @@ INNER JOIN ecommerce.orders o ON u.user_id = o.user_id
 GROUP BY u.name
 HAVING SUM(o.amount) > 1000
 ORDER BY total_gasto DESC;
+
+-- "Lista los países (country) con más de 50 usuarios cuyo promedio de edad sea mayor a 30 años. Muestra país, cantidad de usuarios y edad promedio, ordenado por cantidad de usuarios (desc)."
+SELECT 
+country, 
+COUNT(user_id) AS cantidad_usuarios, 
+AVG(age) 
+FROM ecommerce.users 
+WHERE age IS NOT NULL
+GROUP BY country 
+HAVING COUNT(user_id) > 50 
+AND AVG(age) > 30 
+ORDER BY cantidad_usuarios DESC;
