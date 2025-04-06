2a1dfb16a449f62779b848611a2cbb0c0156c348
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-04-06 14:08:00 -0300

    Agrega consulta de practica

--- 30_practica.sql
+++ 30_practica.sql
@@ -32,3 +32,17 @@ GROUP BY country
 HAVING COUNT(user_id) > 50 
 AND AVG(age) > 30 
 ORDER BY cantidad_usuarios DESC;
+
+-- "Calcula el total de ventas (amount) por mes en 2023, usando solo datos de la partición de ese año. Muestra el mes (formato 'YYYY-MM') y el monto total, ordenado cronológicamente."
+SELECT
+  EXTRACT(YEAR FROM order_date) AS año,
+  EXTRACT(MONTH FROM order_date) AS mes,
+  SUM(amount) AS total_ventas
+FROM 
+  `ecommerce.orders`
+WHERE 
+  _PARTITIONDATE BETWEEN '2023-01-01' AND '2023-12-31'
+GROUP BY 
+  año, mes
+ORDER BY 
+  año, mes;
